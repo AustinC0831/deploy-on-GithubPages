79719de59d61f50ff241e4ea09473f900dae8c9d
AustinC0831 <anonytp92@gmail.com>
2022-12-26 17:52:18 +0800

    bbb

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,18 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-const instance = axios.create({
-  baseURL: `http://localhost:4000/`,
-});
+// const instance = axios.create({
+//   baseURL: `http://localhost:4000/`,
+// });
 
-export default instance;
+// export default instance;
 
 // instance.get('/hi').then((data) => console.log(data));
+
+import axios from "axios";
+
+const API_ROOT =
+  process.env.NODE_ENV === "production"
+    ? "/"
+    : "http://localhost:4000/";
+
+export const api = axios.create({ baseURL: API_ROOT });
